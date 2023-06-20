e8b54149080bc823bd285cd64ad79651ae850762
Николаев Андрей <agnikolaev@beeline.ru>
2023-06-20 10:30:02 +0300

    Изменен файл run_init_db.sh

--- sql/init_db/run_init_db.sh
+++ sql/init_db/run_init_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name demo-pg-13.3 -p 5432:5432 -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -d -v "$(pwd)":/docker-entrypoint-initdb.d postgres:13.3
+docker run  --name demo-pg-13.3 -p 5432:5432 -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -d -v "$(pwd)":/docker-entrypoint-initdb.d postgres:13.3
