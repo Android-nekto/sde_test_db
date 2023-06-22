627fc50d7d3723262973e870d6f75cf2d558f648
Николаев Андрей <agnikolaev@beeline.ru>
2023-06-22 17:45:15 +0300

    убрал sudo

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-# Скачиваем Docker образ PostgreSQL. Не нужно, т.к. скачает, если не найдет локально.
-#docker pull postgres:latest
-
-# Путь к репозиторию определяю как  переменную
-path=`realpath sde_test_db*`
-echo $path
-
 # Запускаем контейнер PostgreSQL. Можно без указания пути -v echo $path:echo $path
-docker run --name  postgres-container -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER="test_sde" -e POSTGRES_DB="demo" -p 5432:5432 -d postgres
-
-# Выводим информацию о базе данных в контейнере PostgreSQL
-docker exec postgres-container psql -U test_sde -d demo -c "SELECT * FROM pg_database"
-
-# Путь к запускаемому файлу определяю как переменную. Копируем файл для заполнения БД в контейнер (так как при каждом запуске папка очищается)
-pathsql=`find $(pwd) -name demo.sql` && echo $pathsql  && docker cp echo $pathsql postgres-container:/var/lib/postgresql/data/
+docker run --name sde-pg -p 5432:5432 --rm -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -e PGDATA=/var/lib/postgresql/data/pgdata -v /Users/AGNikolaev/Desktop/learning/sde_test_db/sql:/var/lib/postgresql/data -d postgres
 
 # Даем время на внесение изменений
-sleep 5
+sleep 20
 
 #  Запускаем скрипт для заполнения БД
-docker exec postgres-container psql -U test_sde -d demo -f //var/lib/postgresql/data/demo.sql
+docker exec sde-pg psql -U test_sde -d demo -f /var/lib/postgresql/data/init_db/demo.sql
 
 # Выводим сообщение об успешном завершении скрипта
 echo "Инициализация БД прошла успешна."
+
