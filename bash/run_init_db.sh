a96e6e316bc9b77fda271d8495d93cf6b0cdab78
Николаев Андрей <agnikolaev@beeline.ru>
2023-06-22 18:30:55 +0300

    убрал sudo

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Запускаем контейнер PostgreSQL. Можно без указания пути -v echo $path:echo $path
-docker run --name sde-pg -p 5432:5432 --rm -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -e PGDATA=C:/Users/AGNikolaev/Desktop/learning/sde_test_db:/var/lib/postgresql/data/pgdata -v C:/Users/AGNikolaev/Desktop/learning/sde_test_db/sql:/var/lib/postgresql/data -d postgres
+#docker run --name sde-pg -p 5432:5432 --rm -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -e PGDATA=C:/Users/AGNikolaev/Desktop/learning/sde_test_db:/var/lib/postgresql/data/pgdata -v C:/Users/AGNikolaev/Desktop/learning/sde_test_db/sql:/var/lib/postgresql/data -d postgres
 
 # Даем время на внесение изменений
-sleep 20
+#sleep 20
 
 #  Запускаем скрипт для заполнения БД
 docker exec sde-pg psql -U test_sde -d demo -f /var/lib/postgresql/data/init_db/demo.sql
-
+sleep 10
 
 # Выводим сообщение об успешном завершении скрипта
 echo "Инициализация БД прошла успешна."
+
+
 
